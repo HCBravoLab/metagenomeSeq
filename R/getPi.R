f7060c992c9fbc1242716ff1886b851c23752394
Marc Carlson <mcarlson@fhcrc.org>
2013-03-25 18:40:33 +0000

    Adds DASiR, metagenomeSeq to the repos and manifest.
    git-svn-id: file:///home/git/hedgehog.fhcrc.org/bioconductor/trunk/madman/Rpacks/metagenomeSeq@74804 bc3139a8-67e5-0310-9ffc-ced21a209358

---
+++ R/getPi.R
@@ -0,0 +1,15 @@
+#' Calculate the mixture proportions from the zero model / spike mass model residuals.
+#'
+#' F(x) = 1 / (1 + exp(-(x-m)/s)) (the CDF of the logistic distribution).
+#' Provides the probability that a real-valued random variable X with a given probability distribution will be found at a value less than or equal to x.
+#' The output are the mixture proportions for the samples given the residuals from the zero model.
+#'
+#' @param residuals Residuals from the zero model.
+#' @return Mixture proportions for each sample.
+#'
+#' @name getPi
+#' @seealso \code{\link{fitZig}}
+getPi <-
+function(residuals){
+	plogis(residuals)
+}
---
+++ R/load_phenoData.R
@@ -0,0 +1,51 @@
+#' Load a clinical/phenotypic dataset associated with a study.
+#'
+#' Load a matrix of metadata associated with a study.
+#'
+#' @param file Path and filename of the actual clinical file.
+#' @param tran Boolean. If the covariates are along the columns and samples along the rows, then tran should equal TRUE.
+#' @param sep The separator for the file.
+#' @return The metadata as a dataframe.
+#'
+#' @name load_phenoData
+#' @aliases phenoData
+#' @seealso \code{\link{load_meta}}
+#' @examples
+#' clin = load_phenoData("~/Desktop/testFile.tsv")
+
+load_phenoData <-
+function(file,tran=FALSE,sep="\t")
+{
+	dat2 <- read.table(file,header=FALSE,sep=sep);
+
+# no. of subjects 
+	subjects <- array(0,dim=c(ncol(dat2)-1));
+	for(i in 1:length(subjects)) {
+		subjects[i] <- as.character(dat2[1,i+1]);
+	}
+# no. of rows
+	rows <- nrow(dat2);
+	
+# load remaining counts
+	matrix <- array(NA, dim=c(length(subjects),rows-1));
+	covar = array(NA,dim=c(rows-1,1)); 
+	
+	for(i in 1:(rows)-1){
+		for(j in 1:(length(subjects))){ 
+			matrix[j,i] <- as.character(dat2[i+1,j+1]);
+		}
+		covar[i] = as.character(dat2[i+1,1]);
+	}  
+	
+		
+	phenoData<-as.data.frame(matrix);
+	
+	colnames(phenoData) = covar;
+	if(length(unique(subjects))==(length(subjects))){
+		rownames(phenoData) = subjects;
+	}
+    if(tran==TRUE){
+        phenoData = as.data.frame(t(phenoData))
+	}
+	return(phenoData);
+}
