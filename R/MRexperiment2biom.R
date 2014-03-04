8616962e564c86417d1be3bf216e69dfbd0a0a10
j.paulson <j.paulson@bc3139a8-67e5-0310-9ffc-ced21a209358>
2014-03-04 07:26:28 +0000

    Commit made by the Bioconductor Git-SVN bridge. Consists of 2 commit(s).
    Commit information:
    
        Commit id: 252a7e23fde3c29a0e343308a66a56bdc7054203
        Commit message:
        Added MRexperiment2biom - now files can be converted/written out to biom format.
        Committed by nosson <nosson at gmail.com>
        Commit date: 2014-03-03T23:26:05-08:00
    
        Commit id: adead6fb5ea9d4bc1dd235055b92f2e7d82d468b
        Commit message:
        Merge branch 'master' of https://github.com/nosson/metagenomeSeq
        Committed by nosson <nosson at gmail.com>
        Commit date: 2014-03-03T23:26:12-08:00
    
    From: Bioconductor Git-SVN Bridge <bioc-sync@bioconductor.org>
    
    git-svn-id: https://hedgehog.fhcrc.org/bioconductor/trunk/madman/Rpacks/metagenomeSeq@86997 bc3139a8-67e5-0310-9ffc-ced21a209358

---
+++ R/MRexperiment2biom.R
@@ -0,0 +1,42 @@
+#' MRexperiment to biom objects
+#' 
+#' Wrapper to convert MRexperiment objects to biom objects.
+#' 
+#' @param obj The MRexperiment object.
+#' @param id Optional id for the biom matrix.
+#' @return A biom object.
+#' @seealso \code{\link{load_meta}} \code{\link{load_phenoData}} \code{\link{newMRexperiment}} \code{\link{load_biom}} \code{\link{biom2MRexperiment}}
+MRexperiment2biom <- function(obj,id=NULL){
+	library(biom)
+	id = id
+	format = "Biological Observation Matrix 0.9.1-dev"
+	format_url = "http://biom-format.org/documentation/format_versions/biom-1.0.html"
+	type = "OTU table"
+	generated_by = sprintf("metagenomeSeq %s",packageVersion("metagenomeSeq"))
+	date = as.character(Sys.time())
+	matrix_type = "dense"
+	matrix_element_type = "int"
+
+	data = MRcounts(obj)
+	shape = dim(data)
+
+	# Eventually we will provide options for users to input their metadata.
+	# Probably not that difficult, maybe metadata = lapply(1:ncol(pData(obj)),function(j)){pData(obj)[i,j]})
+	rows = lapply(1:nrow(data),function(i){list(
+				id=rownames(data)[i],
+				metadata=lapply(1:ncol(fData(obj)),function(j){
+					as.character(fData(obj)[i,j])}))
+			})
+	columns  = lapply(1:ncol(data),function(i){list(
+				id=colnames(data)[i],
+				metadata=lapply(1:ncol(pData(obj)),function(j){
+					as.character(pData(obj)[i,j])}))
+			})
+	data = as.list(as.data.frame(t(data)))
+	names(data) <- NULL
+	
+	biomlist = list(id=id,format=format,format_url=format_url,type=type,generated_by=generated_by,
+					date=date,matrix_type=matrix_type,matrix_element_type=matrix_element_type,shape=shape,
+					rows=rows,columns=columns,data=data)
+	biom(biomlist)
+}
