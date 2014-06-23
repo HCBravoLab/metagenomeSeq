d25b22ef30087096bf10671e796a504e5e034596
j.paulson <j.paulson@bc3139a8-67e5-0310-9ffc-ced21a209358>
2014-06-23 18:35:50 +0000

    Commit made by the Bioconductor Git-SVN bridge. Consists of 1 commit.
    Commit information:
    
    Commit id: bc502d7aa3919a874f9221631ab95f9e9d4fb7ba
    Commit message: update plotMRheatmap
    Committed by: nosson
    Author Name: nosson
    Commit date: 2014-06-23 14:35:40 -0400
    Author date: 2014-06-23 14:35:40 -0400
    
    
    
    git-svn-id: https://hedgehog.fhcrc.org/bioconductor/trunk/madman/Rpacks/metagenomeSeq@91756 bc3139a8-67e5-0310-9ffc-ced21a209358

--- R/plotMRheatmap.R
+++ R/plotMRheatmap.R
@@ -1,13 +1,14 @@
 #' Basic heatmap plot function for normalized counts.
 #' 
-#' This function plots a heatmap of the "n" features with greatest variance
-#' across rows.
+#' This function plots a heatmap of the 'n' features with greatest variance
+#' across rows (or other statistic).
 #' 
 #' 
 #' @param obj A MRexperiment object with count data.
-#' @param n The number of features to plot. This chooses the "n" features with greatest variance.
+#' @param n The number of features to plot. This chooses the 'n' features of greatest positive statistic.
 #' @param log Whether or not to log2 transform the counts - if MRexperiment object.
 #' @param norm Whether or not to normalize the counts - if MRexperiment object.
+#' @param fun Function to select top 'n' features.
 #' @param ... Additional plot arguments.
 #' @return NA
 #' @seealso \code{\link{cumNormMat}}
@@ -17,21 +18,25 @@
 #' trials = pData(mouseData)$diet
 #' heatmapColColors=brewer.pal(12,"Set3")[as.integer(factor(trials))];
 #' heatmapCols = colorRampPalette(brewer.pal(9, "RdBu"))(50)
+#' #### version using sd
 #' plotMRheatmap(obj=mouseData,n=200,cexRow = 0.4,cexCol = 0.4,trace="none",
 #'              col = heatmapCols,ColSideColors = heatmapColColors)
-#' 
-plotMRheatmap <- function(obj,n,log=TRUE,norm=TRUE,...) {
-    if(class(obj)=="MRexperiment"){
-        mat = MRcounts(obj,norm=norm,log=log)
-    } else if(class(obj) == "matrix") {
-        mat = obj
-    } else {
-       stop("Object needs to be either a MRexperiment object or matrix")
-    }
+#' #### version using MAD
+#' plotMRheatmap2(obj=mouseData,n=50,fun=mad,cexRow = 0.4,cexCol = 0.4,trace="none",
+#'              col = heatmapCols,ColSideColors = heatmapColColors) 
+#'
+plotMRheatmap <- function(obj,n,log=TRUE,norm=TRUE,fun=sd,...) {
+  if(class(obj)=="MRexperiment"){
+    mat = MRcounts(obj,norm=norm,log=log)
+  } else if(class(obj) == "matrix") {
+    mat = obj
+  } else {
+    stop("Object needs to be either a MRexperiment object or matrix")
+  }
     
-  otusToKeep <- which(rowSums(mat)>0);
-  otuVars=rowSds(mat[otusToKeep,]);
-  otuIndices=otusToKeep[order(otuVars,decreasing=TRUE)[1:n]];
+  otusToKeep = which(rowSums(mat)>0);
+  otuStats   = apply(mat[otusToKeep,],1,fun);
+  otuIndices = otusToKeep[order(otuStats,decreasing=TRUE)[1:n]];
   mat2=mat[otuIndices,];
   heatmap.2(mat2,...);
   invisible()
