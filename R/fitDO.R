d695f1f7ac3d6745399466d267735e40b207509d
nosson <nosson@gmail.com>
2014-08-03 10:43:35 -0400

    fixing namespace and cleaning parallel code in fitPA,DO and correlationTest

--- R/fitDO.R
+++ R/fitDO.R
@@ -12,11 +12,10 @@
 #' @param cl Group comparison
 #' @param norm Whether or not to normalize the counts - if MRexperiment object.
 #' @param log Whether or not to log2 transform the counts - if MRexperiment object.
-#' @param parallel Use multiple cores?
-#' @param cores Number of cores to use.
 #' @param adjust.method Method to adjust p-values by. Default is "FDR". Options
 #' include "holm", "hochberg", "hommel", "bonferroni", "BH", "BY", "fdr",                                                                                                                            
 #' "none". See \code{\link{p.adjust}} for more details.
+#' @param cores Number of cores to use.
 #' @param ... Extra options for makeCluster
 #' @return Matrix of odds ratios, p-values, lower and upper confidence intervals
 #' @seealso \code{\link{cumNorm}} \code{\link{fitZig}} \code{\link{fitPA}} \code{\link{fitMeta}}
@@ -29,7 +28,7 @@
 #' res = fitDO(lungTrim,pData(lungTrim)$SmokingStatus);
 #' head(res)
 #' 
-fitDO<-function(obj,cl,norm=TRUE,log=TRUE,parallel=FALSE,cores=2,adjust.method='fdr',...){
+fitDO<-function(obj,cl,norm=TRUE,log=TRUE,adjust.method='fdr',cores=1,...){
     if(class(obj)=="MRexperiment"){
         x = MRcounts(obj,norm=norm,log=log);
     } else if(class(obj) == "matrix") {
@@ -42,47 +41,29 @@ fitDO<-function(obj,cl,norm=TRUE,log=TRUE,parallel=FALSE,cores=2,adjust.method='
 
     sumClass1 = round(sum(x[,cl==unique(cl)[1]]))
     sumClass2 = round(sum(x[,cl==unique(cl)[2]]))
-    if(parallel==FALSE){
-        res = sapply(1:nrows,function(i){
-            tbl = table(1-x[i,],cl)
+    
+    cores <- makeCluster(getOption("cl.cores", cores),...)
+    res = parRapply(cl=cores,x,function(i){
+            tbl = table(1-i,cl)
             if(sum(dim(tbl))!=4){
                 tbl = array(0,dim=c(2,2));
-                tbl[1,1] = round(sum(x[i,cl==unique(cl)[1]]))
-                tbl[1,2] = round(sum(x[i,cl==unique(cl)[2]]))
+                tbl[1,1] = round(sum(i[cl==unique(cl)[1]]))
+                tbl[1,2] = round(sum(i[cl==unique(cl)[2]]))
                 tbl[2,1] = sumClass1-tbl[1,1]
                 tbl[2,2] = sumClass2-tbl[1,2]
             }
-            ft <- fisher.test(tbl, workspace = 8e6, alternative = "two.sided", conf.int = TRUE)
-            cbind(o=ft$estimate,cl=ft$conf.int[1],cu=ft$conf.int[2],p=ft$p.value)
+            ft <- fisher.test(tbl,workspace=8e6,alternative="two.sided",conf.int=TRUE)
+            cbind(p=ft$p.value,o=ft$estimate,cl=ft$conf.int[1],cu=ft$conf.int[2])
         })
-        res = data.frame(as.matrix(t(res)))
-        adjp = p.adjust(res[,4],method=adjust.method)
-        res = data.frame(cbind(res,adjp))
-    } else {
-        library(parallel)
-        cores <- makeCluster(getOption("cl.cores", cores))
-        res = parRapply(cl=cores,x,function(i){
-                tbl = table(1-i,cl)
-                if(sum(dim(tbl))!=4){
-                    tbl = array(0,dim=c(2,2));
-                    tbl[1,1] = round(sum(i[cl==unique(cl)[1]]))
-                    tbl[1,2] = round(sum(i[cl==unique(cl)[2]]))
-                    tbl[2,1] = sumClass1-tbl[1,1]
-                    tbl[2,2] = sumClass2-tbl[1,2]
-                }
-                ft <- fisher.test(tbl,workspace=8e6,alternative="two.sided",conf.int=TRUE)
-                cbind(p=ft$p.value,o=ft$estimate,cl=ft$conf.int[1],cu=ft$conf.int[2])
-            })
-        stopCluster(cores)
-        nres = nrows*4
-        seqs = seq(1,nres,by=4)
-        p = res[seqs]
-        adjp = p.adjust(p,method=adjust.method)
-        o = res[seqs+1]
-        cl = res[seqs+2]
-        cu = res[seqs+3]
-        res = data.frame(cbind(o,cl,cu,p,adjp))
-    }
+    stopCluster(cores)
+    nres = nrows*4
+    seqs = seq(1,nres,by=4)
+    p = res[seqs]
+    adjp = p.adjust(p,method=adjust.method)
+    o = res[seqs+1]
+    cl = res[seqs+2]
+    cu = res[seqs+3]
+    res = data.frame(cbind(o,cl,cu,p,adjp))
     colnames(res) = c("oddsRatio","lower","upper","pvalues","adjPvalues")
     rownames(res) = rownames(x)
     return(res)
--- R/fitPA.R
+++ R/fitPA.R
@@ -8,12 +8,11 @@
 #' matrix.
 #' @param cl Group comparison
 #' @param thres Threshold for defining presence/absence.
-#' @param parallel Use multiple cores?
-#' @param cores Number of cores to use.
 #' @param adjust.method Method to adjust p-values by. Default is "FDR". Options
 #' include "holm", "hochberg", "hommel", "bonferroni", "BH", "BY", "fdr",
 #' "none". See \code{\link{p.adjust}} for more details.
-#' @param ... Extra options for makeCluster
+#' @param cores Number of cores to use.
+#' @param ... Extra parameters for makeCluster
 #' @return Matrix of odds ratios, p-values, lower and upper confidence intervals
 #' @seealso \code{\link{cumNorm}} \code{\link{fitZig}} \code{\link{fitDO}} \code{\link{fitMeta}}
 #' @examples
@@ -25,7 +24,7 @@
 #' res = fitPA(lungTrim,pData(lungTrim)$SmokingStatus);
 #' head(res)
 #'
-fitPA<-function(obj,cl,thres=0,parallel=FALSE,cores=2,adjust.method='fdr',...){
+fitPA<-function(obj,cl,thres=0,adjust.method='fdr',cores=1,...){
     if(class(obj)=="MRexperiment"){
         x = MRcounts(obj)>thres;
     } else if(class(obj) == "matrix") {
@@ -39,47 +38,29 @@ fitPA<-function(obj,cl,thres=0,parallel=FALSE,cores=2,adjust.method='fdr',...){
     nClass1 = sum(cl==unique(cl)[1])
     nClass2 = sum(cl==unique(cl)[2])
 
-    if(parallel==FALSE){
-        res = sapply(1:nrows,function(i){
-            tbl = table(1-x[i,],cl)
+    cores <- makeCluster(getOption("cl.cores", cores),...)
+    res = parRapply(cl=cores,x,function(i){
+            tbl = table(1-i,cl)
             if(sum(dim(tbl))!=4){
                 tbl = array(0,dim=c(2,2));
-                tbl[1,1] = sum(x[i,cl==unique(cl)[1]])
-                tbl[1,2] = sum(x[i,cl==unique(cl)[2]])
+                tbl[1,1] = sum(i[cl==unique(cl)[1]])
+                tbl[1,2] = sum(i[cl==unique(cl)[2]])
                 tbl[2,1] = nClass1-tbl[1,1]
                 tbl[2,2] = nClass2-tbl[1,2]
             }
             ft <- fisher.test(tbl,workspace=8e6,alternative="two.sided",conf.int=TRUE)
             cbind(o=ft$estimate,cl=ft$conf.int[1],cu=ft$conf.int[2],p=ft$p.value)
         })
-        res = data.frame(as.matrix(t(res)))
-        adjp = p.adjust(res[,4],method=adjust.method)
-        res = data.frame(cbind(res,adjp))
-    } else {
-        library(parallel)
-        cores <- makeCluster(getOption("cl.cores", cores))
-        res = parRapply(cl=cores,x,function(i){
-                tbl = table(1-i,cl)
-                if(sum(dim(tbl))!=4){
-                    tbl = array(0,dim=c(2,2));
-                    tbl[1,1] = sum(i[cl==unique(cl)[1]])
-                    tbl[1,2] = sum(i[cl==unique(cl)[2]])
-                    tbl[2,1] = nClass1-tbl[1,1]
-                    tbl[2,2] = nClass2-tbl[1,2]
-                }
-                ft <- fisher.test(tbl,workspace=8e6,alternative="two.sided",conf.int=TRUE)
-                cbind(o=ft$estimate,cl=ft$conf.int[1],cu=ft$conf.int[2],p=ft$p.value)
-            })
-        stopCluster(cores)
-        nres = nrows*4
-        seqs = seq(1,nres,by=4)
-        p = res[seqs+3]
-        adjp = p.adjust(p,method=adjust.method)
-        o = res[seqs]
-        cl = res[seqs+1]
-        cu = res[seqs+2]
-        res = data.frame(cbind(o,cl,cu,p,adjp))
-    }
+    stopCluster(cores)
+    nres = nrows*4
+    seqs = seq(1,nres,by=4)
+    p = res[seqs+3]
+    adjp = p.adjust(p,method=adjust.method)
+    o = res[seqs]
+    cl = res[seqs+1]
+    cu = res[seqs+2]
+    res = data.frame(cbind(o,cl,cu,p,adjp))
+
     colnames(res) = c("oddsRatio","lower","upper","pvalues","adjPvalues")
     rownames(res) = rownames(x)
     return(res)
