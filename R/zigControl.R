0188ffe0e39a7d0f4b72c67bfe6f747b66586cb3
nosson <nosson@gmail.com>
2014-11-17 16:10:39 -0500

    adding alternate df definition

--- R/zigControl.R
+++ R/zigControl.R
@@ -3,6 +3,7 @@
 #' @param tol The tolerance for the difference in negative log likelihood estimates for a feature to remain active.
 #' @param maxit The maximum number of iterations for the expectation-maximization algorithm.
 #' @param verbose Whether to display iterative step summary statistics or not.
+#' @param dfmethod Either 'default' or 'modified' (by responsibilities)
 #' @return The value for the tolerance, maximum no. of iterations, and the verbose warning.
 #' @note \code{\link{fitZig}} makes use of zigControl.
 #'
@@ -12,7 +13,7 @@
 #' @examples
 #' control =  zigControl(tol=1e-10,maxit=10,verbose=FALSE)
 #'
-zigControl <-function(tol=1e-4,maxit=10,verbose=TRUE){
-	set <-list(tol=tol,maxit=maxit,verbose=verbose);
+zigControl <-function(tol=1e-4,maxit=10,verbose=TRUE,dfmethod="default"){
+	set <-list(tol=tol,maxit=maxit,verbose=verbose,dfmethod=dfmethod);
 	return(set)	
 }
