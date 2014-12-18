ab379b209bda494c1e9cc88feabeb30d977c7ab9
nosson <nosson@gmail.com>
2014-12-17 19:32:30 -0500

    Fixed MRexperiment2biom if lacking pdata/fdata

--- R/MRexperiment2biom.R
+++ R/MRexperiment2biom.R
@@ -7,65 +7,70 @@
 #' @param norm Normalized data?
 #' @param log Logged data?
 #' @param sl scaling factor for normalized counts.
-#' @param qiimeVersion Format fData according to QIIME specifications (assumes only taxonomy in fData).
 #' @return A biom object.
 #' @seealso \code{\link{load_meta}} \code{\link{load_phenoData}} \code{\link{newMRexperiment}} \code{\link{load_biom}} \code{\link{biom2MRexperiment}}
-MRexperiment2biom <- function(obj,id=NULL,norm=FALSE,log=FALSE,sl=1000,qiimeVersion=TRUE){
-	library(biom)
-	id = id
-	format = "Biological Observation Matrix 1.0.0-dev"
-	format_url = "http://biom-format.org/documentation/format_versions/biom-1.0.html"
-	type = "OTU table"
-	generated_by = sprintf("metagenomeSeq %s",packageVersion("metagenomeSeq"))
-	date = as.character(Sys.time())
-	matrix_type = "dense"
-	matrix_element_type = "int"
-	if( (norm==TRUE) | (log == TRUE) ) {
-		matrix_element_type = "float"
-	}
-	
-	data = MRcounts(obj,norm=norm,log=log,sl=sl)
-	shape = dim(data)
+MRexperiment2biom <- function(obj,id=NULL,norm=FALSE,log=FALSE,sl=1000){
+    library(biom)
+    id = id
+    format = "Biological Observation Matrix 1.0.0-dev"
+    format_url = "http://biom-format.org/documentation/format_versions/biom-1.0.html"
+    type = "OTU table"
+    generated_by = sprintf("metagenomeSeq %s",packageVersion("metagenomeSeq"))
+    date = as.character(Sys.time())
+    matrix_type = "dense"
+    matrix_element_type = "int"
+    if( (norm==TRUE) | (log == TRUE) ) {
+        matrix_element_type = "float"
+    }
+    
+    data  = MRcounts(obj,norm=norm,log=log,sl=sl)
+    shape = dim(data)
 
-	fdatanames = colnames(fData(obj))
-	# this is a bad hack - can make better
-	if(qiimeVersion==TRUE){
-		fd = fData(obj)
-		for(i in 1:ncol(fd)){
-			fd[,i] = as.character(fd[,i])
-		}
-		rows = lapply(1:nrow(data),function(i){
-		ll = list(
-			id=rownames(data)[i],
-			metadata=list("taxonomy" = paste(fd[i,]))
-			)
-		ll
-		})
+    rows   = metadata(fData(obj),qiimeVersion=qiimeVersion)
+    columns= metadata(pData(obj))
 
-	} else {
-		rows = lapply(1:nrow(data),function(i){
-				ll = list(
-					id=rownames(data)[i],
-					metadata=lapply(1:ncol(fData(obj)),function(j){
-						as.character(fData(obj)[i,j])}))
-				names(ll$metadata) = fdatanames
-				ll
-				})
-	}
-	sdatanames = colnames(pData(obj))
-	columns  = lapply(1:ncol(data),function(i){
-			ll = list(
-				id=colnames(data)[i],
-				metadata=lapply(1:ncol(pData(obj)),function(j){
-					as.character(pData(obj)[i,j])}))
-			names(ll$metadata) = sdatanames
-			ll
-			})
-	data = as.list(as.data.frame(t(data)))
-	names(data) <- NULL
-	
-	biomlist = list(id=id,format=format,format_url=format_url,type=type,generated_by=generated_by,
-					date=date,matrix_type=matrix_type,matrix_element_type=matrix_element_type,shape=shape,
-					rows=rows,columns=columns,data=data)
-	biom::biom(biomlist)
+    data = as.list(as.data.frame(t(data)))
+    names(data) <- NULL
+    
+    biomlist = list(id=id,format=format,format_url=format_url,type=type,generated_by=generated_by,
+                    date=date,matrix_type=matrix_type,matrix_element_type=matrix_element_type,shape=shape,
+                    rows=rows,columns=columns,data=data)
+    biom::biom(biomlist)
+}
+
+metadata<-function(df,qiimeVersion=FALSE){
+    if(ncol(df)>0){
+        for(i in 1:ncol(df)){
+            df[,i] = as.character(df[,i])
+        }
+    }
+    if(qiimeVersion==TRUE){
+        if(ncol(df)==0){
+            meta = lapply(1:nrow(df),function(i){
+                    ll = list(id=rownames(df)[i],metadata=NULL)
+                    ll
+                })
+        } else {
+        	meta = lapply(1:nrow(df),function(i){
+            	    ll = list(id=rownames(df)[i], 
+	                metadata=list("taxonomy" = paste(df[i,])))
+                	ll
+            	})
+        }
+    } else {
+        if(ncol(df)==0){
+            meta = lapply(1:nrow(df),function(i){
+                    ll = list(id=rownames(df)[i],metadata=NULL)
+                    ll
+                })
+        } else {
+            meta  = lapply(1:nrow(df),function(i){
+                    ll = list(id=rownames(df)[i],
+                    metadata=lapply(1:ncol(df),
+                        function(j){as.character(df[i,j])}))
+                    names(ll$metadata) = colnames(df)
+                    ll
+                })            
+        }
+    }
 }
