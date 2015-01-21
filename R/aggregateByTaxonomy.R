4144cbaa045b510dfc8d83d50068e7e6a0d7c81f
Joseph Paulson <jpaulson@umiacs.umd.edu>
2015-01-21 06:15:51 +0000

    Commit made by the Bioconductor Git-SVN bridge. Consists of 1 commit.
    Commit information:
    
    Commit id: 384d1491ac0ebcb21e634d8274ab1974244270bd
    
        adding aggregateBySample
    
    Committed by: nosson
    Author Name: nosson
    Commit date: 2015-01-21 01:15:42 -0500
    Author date: 2015-01-21 01:15:42 -0500
    
    
    git-svn-id: file:///home/git/hedgehog.fhcrc.org/bioconductor/trunk/madman/Rpacks/metagenomeSeq@98545 bc3139a8-67e5-0310-9ffc-ced21a209358

--- R/aggregateByTaxonomy.R
+++ R/aggregateByTaxonomy.R
@@ -76,3 +76,69 @@ aggregateByTaxonomy<-function(obj,lvl,alternate=FALSE,norm=FALSE,log=FALSE,aggfu
 aggTax<-function(obj,lvl,alternate=FALSE,norm=FALSE,log=FALSE,aggfun = colSums,sl=1000,out='MRexperiment'){
 	aggregateByTaxonomy(obj,lvl,alternate=alternate,norm=norm,log=log,aggfun = aggfun,sl=sl,out=out)
 }
+
+#' @name aggregateBySample
+#' @title Aggregates a MRexperiment object or counts matrix to by a factor.
+#' 
+#' @details Using the phenoData information in the MRexperiment, calling aggregateBySample on a
+#' MRexperiment and a particular phenoData column (i.e. 'diet') will aggregate counts
+#' using the aggfun function (default rowMeans). Possible aggfun alternatives
+#' include rowMeans and rowMedians.
+#' 
+#' @param obj A MRexperiment object or count matrix.
+#' @param fct phenoData column name from the MRexperiment object or if count matrix object a vector of labels.
+#' @param aggfun Aggregation function.
+#' @param out Either 'MRexperiment' or 'matrix'
+#' @return An aggregated count matrix or MRexperiment object.
+#' @aliases aggSamp
+#' @rdname aggregateBySample
+#' @export
+#' @examples
+#' 
+#' data(mouseData)
+#' aggregateBySample(mouseData[1:100,],fct="diet",aggfun=rowSums)
+#' # not run
+#' # aggregateBySample(mouseData,fct="diet",aggfun=rowMedians)
+#' # aggSamp(mouseData,fct='diet',aggfun=rowMaxs)
+#' 
+aggregateBySample<-function(obj,fct,aggfun=rowMeans,out="MRexperiment"){
+	if(class(obj)=="MRexperiment"){
+		mat = MRcounts(obj)
+		if(length(fct)==1) factors = as.character(pData(obj)[,fct])
+		else factors = as.character(fct)
+	} else {
+		mat = obj
+		factors = as.character(fct)
+		if(length(factors)!=ncol(mat)) stop("If input is a count matrix, fct must be a vector of length = ncol(count matrix)")
+	}
+	if(!(out%in%c("MRexperiment","matrix"))){
+		stop("The variable out must either be 'MRexperiment' or 'matrix'")
+	}
+	grps = split(seq_along(factors),factors)
+	
+	newMat = array(NA,dim=c(nrow(obj),length(grps)))
+	for(i in 1:length(grps)){
+		newMat[,i] = aggfun(mat[,grps[[i]],drop=FALSE])
+	}
+	colnames(newMat) = names(grps)
+	rownames(newMat) = rownames(obj)
+	if(out=='matrix') return(newMat)
+	if(out=='MRexperiment'){
+		pd = data.frame(names(grps))
+		colnames(pd) = "pd"
+		rownames(pd) = names(grps)
+		pd = as(pd,"AnnotatedDataFrame")
+		if(class(obj)=="MRexperiment"){
+			fd = as(fData(obj),"AnnotatedDataFrame")
+			newObj = newMRexperiment(newMat,featureData=fd,phenoData=pd)
+		} else {
+			newObj = newMRexperiment(newMat,phenoData=pd)
+		}
+		return(newObj)
+	}
+}
+#' @rdname aggregateBySample
+#' @export
+aggSamp<-function(obj,fct,aggfun=rowMeans,out='MRexperiment'){
+	aggregateBySample(obj,fct,aggfun=aggfun,out=out)
+}
