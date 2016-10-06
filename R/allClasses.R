bff7599ec6c59539f5348c279ded57dbea53a50c
h.corradabravo <h.corradabravo@bc3139a8-67e5-0310-9ffc-ced21a209358>
2016-10-06 18:53:27 +0000

    Merge pull request #44 from HCBravoLab/normFactors
    normFactor generics +
    
    From: hcorrada <hcorrada@gmail.com>
    
    git-svn-id: https://hedgehog.fhcrc.org/bioconductor/trunk/madman/Rpacks/metagenomeSeq@121986 bc3139a8-67e5-0310-9ffc-ced21a209358

--- R/allClasses.R
+++ R/allClasses.R
@@ -28,6 +28,106 @@ setMethod("rowMeans", signature="MRexperiment", function (x, ...) {
 setMethod("colMeans", signature="MRexperiment", function (x, ...) {
     callNextMethod(MRcounts(x),...)
 })
+
+#' Access the normalization factors in a MRexperiment object
+#'
+#' Function to access the scaling factors, aka the normalization factors, of
+#' samples in a MRexperiment object.
+#'
+#' @name normFactors
+#' @docType methods
+#' @param object a \code{MRexperiment} object
+#' @return Normalization scaling factors
+#' @author Joseph N. Paulson
+#' @examples
+#'
+#' data(lungData)
+#' head(normFactors(lungData))
+#'
+setGeneric("normFactors",function(object){standardGeneric("normFactors")}) 
+setGeneric("normFactors<-",function(object,value){standardGeneric("normFactors<-")})
+
+setMethod("normFactors", signature="MRexperiment",function(object) {
+   nf <- expSummary(object)$normFactors
+   nf <- unlist(nf)
+   names(nf) <- sampleNames(object)
+   nf
+ })
+
+#' Replace the normalization factors in a MRexperiment object
+#'
+#' Function to replace the scaling factors, aka the normalization factors, of
+#' samples in a MRexperiment object.
+#'
+#' @name normFactors<-
+#' @docType methods
+#' @aliases normFactors<-,MRexperiment,numeric-method normFactors<-
+#' @param object a \code{MRexperiment} object
+#' @param value vector of normalization scaling factors
+#' @return Normalization scaling factors
+#' @author Joseph N. Paulson
+#' @examples
+#'
+#' data(lungData)
+#' head(normFactors(lungData)<- rnorm(1))
+#'
+setReplaceMethod("normFactors", signature=c(object="MRexperiment", value="numeric"),
+  function( object, value ) {
+   pData(object@expSummary$expSummary)$normFactors <- value
+   validObject( object )
+   object
+})
+
+#' Access sample depth of coverage from MRexperiment object
+#'
+#' Access the libSize vector represents the column (sample specific) sums of features,
+#' i.e. the total number of reads for a sample or depth of coverage. It is used by
+#' \code{\link{fitZig}}.
+#'
+#' @name libSize
+#' @docType methods
+#' @param object a \code{MRexperiment} object
+#' @return Library sizes
+#' @author Joseph N. Paulson
+#' @examples
+#'
+#' data(lungData)
+#' head(libSize(lungData))
+#'
+setGeneric("libSize",function(object){standardGeneric("libSize")})
+setGeneric("libSize<-",function(object,value){standardGeneric("libSize<-")})
+
+setMethod("libSize", signature="MRexperiment",function(object) {
+   ls <- expSummary(object)$libSize
+   ls <- unlist(ls)
+   names(ls) <- sampleNames(object)
+   ls
+ })
+
+#' Replace the library sizes in a MRexperiment object
+#'
+#' Function to replace the scaling factors, aka the library sizes, of
+#' samples in a MRexperiment object.
+#'
+#' @name libSize<-
+#' @docType methods
+#' @aliases libSize<-,MRexperiment,numeric-method libSize<-
+#' @param object a \code{MRexperiment} object
+#' @param value vector of library sizes
+#' @return vector library sizes
+#' @author Joseph N. Paulson
+#' @examples
+#'
+#' data(lungData)
+#' head(libSize(lungData)<- rnorm(1))
+#'
+setReplaceMethod("libSize", signature=c(object="MRexperiment", value="numeric"),
+  function( object, value ) {
+   pData(object@expSummary$expSummary)$libSize <- value
+   validObject( object )
+   object
+})
+
 #' Create a MRexperiment object
 #' 
 #' This function creates a MRexperiment object from a matrix or data frame of
@@ -47,7 +147,7 @@ setMethod("colMeans", signature="MRexperiment", function (x, ...) {
 #' @param normFactors normFactors, the normalization factors used in either the
 #' model or as scaling factors of sample counts for each particular sample.
 #' @return an object of class MRexperiment
-#' @author Joseph N Paulson, jpaulson@@umiacs.umd.edu
+#' @author Joseph N Paulson
 #' @examples
 #' 
 #' cnts = matrix(abs(rnorm(1000)),nc=10)
@@ -145,55 +245,7 @@ posteriorProbs <- function( obj ) {
    stopifnot( is( obj, "MRexperiment" ) )
    assayData(obj)[["z"]]
 }
-#' Access the normalization factors in a MRexperiment object
-#'
-#' Function to access the scaling factors, aka the normalization factors, of
-#' samples in a MRexperiment object.
-#'
-#'
-#' @name normFactors
-#' @aliases normFactors,MRexperiment-method normFactors
-#' @docType methods
-#' @param obj a \code{MRexperiment} object.
-#' @return Normalization scaling factors
-#' @author Joseph N. Paulson, jpaulson@@umiacs.umd.edu
-#' @examples
-#'
-#' data(lungData)
-#' head(normFactors(lungData))
-#'
-normFactors <- function( obj ) {
-   stopifnot( is( obj, "MRexperiment" ) )
-   nf <- pData(obj@expSummary$expSummary)[["normFactors"]]
-   nf <- unlist(nf)
-   names(nf) <- sampleNames(obj)
-   nf
-}
-#' Access sample depth of coverage from MRexperiment object
-#'
-#' The libSize vector represents the column (sample specific) sums of features,
-#' i.e. the total number of reads for a sample or depth of coverage. It is used by
-#' \code{\link{fitZig}}.
-#'
-#'
-#' @name libSize
-#' @aliases libSize,MRexperiment-method libSize
-#' @docType methods
-#' @param obj a \code{MRexperiment} object.
-#' @return Library sizes
-#' @author Joseph N. Paulson, jpaulson@@umiacs.umd.edu
-#' @examples
-#'
-#' data(lungData)
-#' head(libSize(lungData))
-#'
-libSize<-function(obj){
-   stopifnot( is( obj, "MRexperiment" ) )
-   ls <- pData(obj@expSummary$expSummary)[["libSize"]]
-   ls <- unlist(ls)
-   names(ls) <- sampleNames(obj)
-   ls
-}
+
 #' Access MRexperiment object experiment data
 #' 
 #' The expSummary vectors represent the column (sample specific) sums of
